f85b38ddf9c25d035d5813755225225e31e05991
Jordan Foo <foo.jordan@gmail.com>
2018-02-18 01:13:17 -0800

    Working config

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"github.com/op/go-logging"
+	"io/ioutil"
+	"encoding/json"
+	"github.com/chomey/go-slack-bot/config"
 )
 
+var log = logging.MustGetLogger("go-slack-bot")
+
 func main() {
-	fmt.Println("Hello.")
+	log.Info("=====")
+
+	data, err := ioutil.ReadFile("config.json")
+	if err != nil {
+		check(err)
+	}
+
+	var config config.Config
+	json.Unmarshal(data, &config)
+
+	fmt.Println(fmt.Sprintf("Hello %s.", config.Name))
+}
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
